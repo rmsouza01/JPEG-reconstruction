cdecc72a257943f15c3d91fde4230d37c8f48143
kjychung <kjy.chung@gmail.com>
2019-06-06 11:37:05 -0600

    20190606 update - includes DFT-image network (patches not extracted)

--- Modules/cs_models.py
+++ Modules/cs_models.py
@@ -238,6 +238,51 @@ def unet_block(unet_input, kshape=(3, 3)):
     out = Add()([conv8, unet_input])
     return out
 
+def unet_block_2chnl(unet_input, kshape=(3, 3)):
+    """
+    :param unet_input: Input layer
+    :param kshape: Kernel size
+    :return: 2-channel, complex reconstruction
+    """
+
+    conv1 = Conv2D(48, kshape, activation='relu', padding='same')(unet_input)
+    conv1 = Conv2D(48, kshape, activation='relu', padding='same')(conv1)
+    conv1 = Conv2D(48, kshape, activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, kshape, activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, kshape, activation='relu', padding='same')(conv2)
+    conv2 = Conv2D(64, kshape, activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, kshape, activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, kshape, activation='relu', padding='same')(conv3)
+    conv3 = Conv2D(128, kshape, activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, kshape, activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, kshape, activation='relu', padding='same')(conv4)
+    conv4 = Conv2D(256, kshape, activation='relu', padding='same')(conv4)
+
+    up1 = concatenate([UpSampling2D(size=(2, 2))(conv4), conv3], axis=-1)
+    conv5 = Conv2D(128, kshape, activation='relu', padding='same')(up1)
+    conv5 = Conv2D(128, kshape, activation='relu', padding='same')(conv5)
+    conv5 = Conv2D(128, kshape, activation='relu', padding='same')(conv5)
+
+    up2 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv2], axis=-1)
+    conv6 = Conv2D(64, kshape, activation='relu', padding='same')(up2)
+    conv6 = Conv2D(64, kshape, activation='relu', padding='same')(conv6)
+    conv6 = Conv2D(64, kshape, activation='relu', padding='same')(conv6)
+
+    up3 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv1], axis=-1)
+    conv7 = Conv2D(48, kshape, activation='relu', padding='same')(up3)
+    conv7 = Conv2D(48, kshape, activation='relu', padding='same')(conv7)
+    conv7 = Conv2D(48, kshape, activation='relu', padding='same')(conv7)
+
+    conv8 = Conv2D(2, (1, 1), activation='linear')(conv7)
+    out = Add()([conv8, unet_input])
+    return out
+
 
 def DC_block(rec,mask,sampled_kspace,kspace = False):
     """
@@ -385,7 +430,7 @@ def deep_cascade_unet_ksp_no_dc(depth_str='ki', H=256, W=256, kshape=(3, 3)):
             break;
 
         # Add CNN block
-        layers.append(unet_block(layers[-1], kshape))
+        layers.append(unet_block_2chnl(layers[-1], kshape))
         print("Append U-net block")
 
         if kspace_flag:
